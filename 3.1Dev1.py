8556ebe1d04a17897d9f19d0fd41e850682f75cb
FancyGuitarist <simf3010@gmail.com>
2023-10-04 17:53:35 -0400

    10000 iterations pour DAN
    Brr

--- 3.1Dev1.py
+++ 3.1Dev1.py
@@ -10,9 +10,9 @@ j = complex(0, 1)
 
 m = 1
 e = 2.7182818284590452353602874713527
-t = np.linspace(0, np.pi, 1000)
+t = np.linspace(0, np.pi, 10000)
 
-x = np.linspace(0, a, 1000) # define our grid
+x = np.linspace(0, a, 10000) # define our grid
 
 def penice_a(x, t):
     Penis = (1/np.sqrt(a))*(2*np.sin((np.pi*x)/a) * e**(-j * t) + np.sin((np.pi * x * 2)/a) * e**(-j * 2 * t))
