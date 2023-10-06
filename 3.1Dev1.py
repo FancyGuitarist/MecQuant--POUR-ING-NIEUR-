22b41a70b400d33ffcac7383cdfd0a71536a495b
FancyGuitarist <simf3010@gmail.com>
2023-10-06 02:03:48 -0400

    Y peux-tu finir cte criss de devoir
    Arrrrgh

--- 3.1Dev1.py
+++ 3.1Dev1.py
@@ -4,28 +4,29 @@ from scipy.sparse.linalg import eigs
 from findiff import FinDiff
 import matplotlib.pyplot as plt
 
+pi = np.pi
 a = 20
 j = complex(0, 1)
-m = 1
-e = 2.7182818284590452353602874713527
-t = np.linspace(0, 12, 10)
+m = 9.1093837015e-31
+e = 1.602176634e-19
+t = np.linspace(0, 12, 6)
 x = np.linspace(0, a, 1000) # define our grid
 
 def penice_a(x, t):
     Penis = (1/np.sqrt(a))*(2*np.sin((np.pi*x)/a) * e**(-j * t) + np.sin((np.pi * x * 2)/a) * e**(-j * 2 * t))
     return Penis
 
-#colors = ['blue', 'black', 'orange', 'red', 'purple', 'green']
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
 for i in range(len(t)):
-    ax.plot(x , penice_a(x, t[i]).real**2 + penice_a(x, t[i]).imag**2 , label=f'temps : {t[i]:.2f} sec') #color=colors[i],
+    ax.plot(x , penice_a(x, t[i]), label=f'temps : {t[i]:.2f} sec') #color=colors[i], + abs(penice_a(x, t[i]).imag)
 
 fig.tight_layout()
 fig.subplots_adjust(top=0.8)
 plt.xlabel("Distance [-]")
-plt.ylabel('Densité de Probabilité')
+plt.ylabel("Fonction d'onde")
 ax.tick_params(axis="y",direction="in")
 ax.tick_params(axis="x",direction="in")
 ax.legend()
@@ -39,13 +40,29 @@ def penice_b(x, t):
     return Penis
 
 for i in range(len(t)):
-    ax.plot(x , penice_b(x, t[i]).real**2 + penice_b(x, t[i]).imag**2 , label=f'temps : {t[i]:.2f} sec') # color=colors[i],
+    ax.plot(x , penice_b(x, t[i]), label=f'temps : {t[i]:.2f} sec') # color=colors[i], + abs(penice_b(x, t[i]).imag)
 
 fig.tight_layout()
 fig.subplots_adjust(top=0.8)
 plt.xlabel("Distance [-]")
-plt.ylabel('Densité de Probabilité')
+plt.ylabel("Fonction d'onde")
 ax.tick_params(axis="y",direction="in")
 ax.tick_params(axis="x",direction="in")
 ax.legend()
-fig.savefig(f"GraphB", bbox_inches='tight',dpi=600)
+fig.savefig(f"Graph_B", bbox_inches='tight',dpi=600)
+
+def PENICE(x,t):
+    PENIS = (1/np.sqrt(a))*(2*np.sin((np.pi*x)/a) * e**(-j * t) + np.sin((np.pi * x * 2)/a) * e**(-j * 2 * t)) + (1/np.sqrt(a))*(2*np.sin((np.pi*x)/a) * e**(-j * t) + np.sin((np.pi * x * 3)/a) * e**(-j * 9 * t))
+    return PENIS
+
+for i in range(len(t)):
+    ax.plot(x, PENICE(x, t[i]), label=f'temps : {t[i]:.2f} sec') #+ abs(PENICE(x, t[i]).imag)
+
+fig.tight_layout()
+fig.subplots_adjust(top=0.8)
+plt.xlabel("Distance [-]")
+plt.ylabel("Fonction d'onde")
+ax.tick_params(axis="y",direction="in")
+ax.tick_params(axis="x",direction="in")
+#ax.legend()
+fig.savefig(f"GraphThot", bbox_inches='tight',dpi=600)
--- 4Dev1.py
+++ 4Dev1.py
@@ -1,15 +1,17 @@
 import numpy as np
 from scipy.constants import hbar
+from scipy.sparse.linalg import eigs
+import matplotlib.pyplot as plt
 
 def build_Hmatrix(eigenvalue: float, v: np.ndarray, n: int):
   H = np.pad(-np.identity(n - 1), ((0,1),(1,0)), 'constant') + np.identity(n) * (2 + v) + np.pad(-np.identity(n - 1), ((1,0),(0,1)), 'constant')
   return H
 
 def lambd(m, delta_x):
-  return 1 / (2*m*delta_x**2)
+  return hbar**2 / (2*m*delta_x**2)
 
 A = 1
-N = 3
+N = 100
 deltx = A / (N + 1)
 M = 1
 
@@ -18,11 +20,17 @@ def Energie(M, A, N):
     return Energie
 
 Hmatrix = build_Hmatrix(lambd(M, deltx), np.zeros(N), N)
-print(Hmatrix)
+#print(Hmatrix)
 
-eigenVals = np.linalg.eig(Hmatrix - np.eye(N) * Energie(M, A, N))[0][:]
-print(eigenVals)
+eigenVals, eigenVecs = eigs(Hmatrix + np.eye(N) * Energie(M, A, N))
+#print(eigenVals)
+y = eigenVecs[:, 2].real
+#print(y)
+x = np.linspace(0, A, N)
+plt.plot(x, y)
+plt.show()
 
 sorted_eigenVals = sorted(eigenVals)
-for i in sorted_eigenVals[:5]:
-  print(i*lambd(M, deltx))
+"""for i in sorted_eigenVals[:5]:
+  print(i*lambd(M, deltx))"""
+
